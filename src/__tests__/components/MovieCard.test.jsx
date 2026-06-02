4171082407231f0ce620fe87cd95e619e13bb90c
Archo-Jonh <73595869+Archo-Jonh@users.noreply.github.com>
2026-06-01 22:37:57 -0600

    force automatic jsx runtime in esbuild for tests

--- src/__tests__/components/MovieCard.test.jsx
+++ src/__tests__/components/MovieCard.test.jsx
@@ -1,4 +1,3 @@
-import React from 'react' // eslint-disable-line no-unused-vars
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { describe, it, expect, vi } from 'vitest'
