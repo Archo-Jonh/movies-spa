23cb3b3ad54ec8489b8c489fa98329f9c7210546
Archo-Jonh <73595869+Archo-Jonh@users.noreply.github.com>
2026-06-01 23:21:01 -0600

    fix react import in component tests

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,6 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.js'],
-    transformMode: {
-      web: [/\.[jt]sx$/],
-    },
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html'],
